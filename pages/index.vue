fe6caee709837ba765b1f0c437e23be8ccc95986
luddthree <luddetv@gmail.com>
2024-08-06 13:37:14 +0200

    Update navigation links in index page

--- pages/index.vue
+++ pages/index.vue
@@ -58,13 +58,13 @@
                 <a href="#kontakt"
                     class="bg-transparent border border-black text-black px-8 py-3 rounded hover:bg-black hover:text-white">Kontakt
                     oss</a>
-                <button class="bg-transparent text-black px-8 py-3 rounded hover:underline inline-flex items-center">
+                <a href="/about" class="bg-transparent text-black px-8 py-3 rounded hover:underline inline-flex items-center">
                     Les mer
                     <svg xmlns="http://www.w3.org/2000/svg" height="15" width="15" viewBox="0 0 320 512" class="ml-2">
                         <path
                             d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" />
                     </svg>
-                </button>
+                </a>
             </div>
         </div>
         <div class="flex-1 md:px-32">
