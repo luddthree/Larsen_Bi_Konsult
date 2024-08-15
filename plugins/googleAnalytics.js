88b157f22f499666ce464db7b90600a9a841c5cc
luddthree <luddetv@gmail.com>
2024-08-15 10:33:28 +0200

    fr

--- plugins/googleAnalytics.js
+++ plugins/googleAnalytics.js
@@ -1,18 +1,16 @@
 export default defineNuxtPlugin((nuxtApp) => {
-    if (process.env.NODE_ENV !== 'production') {
-      return
+    if (process.client && process.env.NODE_ENV === 'production') {
+      const gtagId = nuxtApp.$config.public.gtagId;
+  
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push(arguments); }
+      gtag('js', new Date());
+      gtag('config', gtagId);
+  
+      const script = document.createElement('script');
+      script.src = `https://www.googletagmanager.com/gtag/js?id=${gtagId}`;
+      script.async = true;
+      document.head.appendChild(script);
     }
-  
-    const gtagId = nuxtApp.$config.public.gtagId
-  
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
-    gtag('config', gtagId);
-  
-    const script = document.createElement('script');
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${gtagId}`;
-    script.async = true;
-    document.head.appendChild(script);
   });
   
